7b19b0302c999fd6708c4caa82d29f2a798bc374
Caulaglobal <157524740+Caulaglobal@users.noreply.github.com>
2024-02-13 21:49:53 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Forget Password',
       theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 202, 238, 222)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 202, 238, 222)),
         useMaterial3: true,
       ),
       home: ForgetPasswordScreen(),
@@ -29,119 +29,131 @@ class ForgetPasswordScreen extends StatelessWidget {
 
   ForgetPasswordScreen({Key? key}) : super(key: key);
 
+  get child => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[99],
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.arrow_back),
-        ),
-      ),
+      backgroundColor: const Color.fromARGB(255, 233, 242, 239),
       body: Stack(
         children: [
-          Text(
-            "Forgot Password?",
-            style: TextStyle(
-              fontSize: 60,
-              fontWeight: FontWeight.bold,
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.arrow_back),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 11),
+            child: const Text(
+              "Forgot Password?",
+              style: TextStyle(
+                fontSize: 60,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Select which contact details should we use to reset your password',
-                  style: TextStyle(fontSize: 18),
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextFormField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      hintText: 'Enter your email',
-                      prefixIcon: Icon(
-                        Icons.message,
-                        color: Colors.greenAccent,
-                      ),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextFormField(
-                    controller: phoneController,
-                    decoration: InputDecoration(
-                      labelText: 'Phone Number',
-                      hintText: 'Enter your phone number',
-                      prefixIcon: Icon(
-                        Icons.phone,
-                        color: Colors.greenAccent,
-                      ),
-                    ),
-                    keyboardType: TextInputType.phone,
-                  ),
-                ),
-                const SizedBox(height: 20),
-              ],
-            ),
-          ),
-          Positioned(
-            left: 0,
-            right: 10,
-            bottom: 20,
-            child: Padding(
-              padding: const EdgeInsets.only(
-                left: 30,
-                right: 30,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text(
+                'Select which contact details should we use to reset your password',
+                style: TextStyle(fontSize: 18),
               ),
-              child: Container(
-                width: 40,
-                height: 60,
-                color: Color.fromARGB(255, 35, 134, 86),
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Text(
-                    'Continue',
-                    style: TextStyle(
-                      color: Colors.white,
+              const SizedBox(height: 5),
+              Container(
+                width: double.infinity,
+                height: 70,
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 233, 242, 239),
+                          borderRadius: BorderRadius.circular(100)),
+                      child: const Icon(
+                        Icons.email,
+                        color: Color.fromARGB(255, 90, 184, 158),
+                      ),
                     ),
-                  ),
-                  // ElevatedButton(
-                  // onPressed: () {
-                  // Handle password reset logic here
-                  // Check if email or phone is provided and send reset accordingly
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Text("via sms:"),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                height: 70,
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 233, 242, 239),
+                          borderRadius: BorderRadius.circular(100)),
+                      child: const Icon(
+                        Icons.sms_rounded,
+                        color: Color.fromARGB(255, 90, 184, 158),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Text('via Email:'),
+                  
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              child: 
+                Container(
+                  width: 40,
+                  height: 60,
+                  color: const Color.fromARGB(255, 6, 168, 122),
+                  child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Continue',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        // ElevatedButton(
+                        // onPressed: () {
+                        // Handle password reset logic here
+                        // Check if email or phone is provided and send reset accordingly
 
-                  // Navigate to the next screen
-                  //   Navigator.push(
-                  //     context,
-                  //     MaterialPageRoute(
-                  //       builder: (context) => const ResetPasswordScreen(),
-                  //     ),
-                  //   );
-                  // },
-                  // style: ButtonStyle(
-                  //   backgroundColor: MaterialStateProperty.all(null),
-                  // ),
-                  // child: const Text('Continue'),
-                ]),
+                        // Navigate to the next screen
+                        //   Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //       builder: (context) => const ResetPasswordScreen(),
+                        //     ),
+                        //   );
+                        // },
+                        // style: ButtonStyle(
+                        //   backgroundColor: MaterialStateProperty.all(null),
+                        // ),
+                        // child: const Text('Continue'),
+                      ]),
+                ),
               ),
-            ),
+            ]),
           ),
         ],
       ),
